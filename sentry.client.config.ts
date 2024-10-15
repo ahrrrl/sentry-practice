9410523bc90ce9725f592a445c8efaa6809da547
mokoko <guswlso2@naver.com>
2024-10-15 09:04:49 +0900

    sentry-ignore

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -2,15 +2,13 @@
 // The config you add here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: "https://f6db667493adaaa6f3915b25adec4aab@o4508120091066368.ingest.us.sentry.io/4508120103321600",
-
+  dsn: 'https://f6db667493adaaa6f3915b25adec4aab@o4508120091066368.ingest.us.sentry.io/4508120103321600',
+  ignoreErrors: ['Something went wrong ignore!'],
   // Add optional integrations for additional features
-  integrations: [
-    Sentry.replayIntegration(),
-  ],
+  integrations: [Sentry.replayIntegration()],
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
